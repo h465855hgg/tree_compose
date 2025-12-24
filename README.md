e69e0fa90d40c534a096f0a5b2e774781e5977c1
rrzt <124551348+h465855hgg@users.noreply.github.com>
2025-12-25 06:03:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tree_compose
+this is compose tree
--- app/src/main/java/com/example/tree_compose/FileTree.kt
+++ app/src/main/java/com/example/tree_compose/FileTree.kt
@@ -1,5 +1,5 @@
 /*
- * WebIDE - A powerful IDE for Android web development.
+ * TreeCompose - A tree-structured file viewer built with Jetpack Compose
  * Copyright (C) 2025  如日中天  <3382198490@qq.com>
  *
  * This program is free software: you can redistribute it and/or modify
